31931aa1877e9d21de1e569e2e264f9a0466446d
WangYini <yini.wang@tum.de>
2016-06-01 09:58:27 +0200

    propertyNode modify: solve some exceptions&add documentaion

--- src/TUM.CMS.VplControl.Test/Nodes/IfcReadPropertiesNode.cs
+++ src/TUM.CMS.VplControl.Test/Nodes/IfcReadPropertiesNode.cs
@@ -1,5 +1,8 @@
-﻿//This Node is written for potential future use of modify node. 
-//It works to some degree but still need to be refined.
+﻿//Documentation
+//This Node is written for potential future use of modify node. (perhaps) 
+//This Node can be used after IfcFilterNode.
+//After filtering some specific ifc products(eg. IfcWall), 
+//you can use this node to choose one of them(eg. one Wall) and see his owned properties and corresponding values.
 //___Yini Wang
 using System;
 using System.Windows;
@@ -59,7 +62,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
             };
 
             var comboBox = new ComboBox { };
-            comboBox.SelectionChanged += comboBox_SelectionChanged;
+            comboBox.SelectionChanged += comboBox_selectionChanged;
 
             AddControlToNode(scrollViewer);
             AddControlToNode(new Label { Content = "IfcReadPropertiesNode" });
@@ -80,19 +83,27 @@ namespace TUM.CMS.VplControl.Test.Nodes
             var list = InputPorts[0].Data as IList;
             if (list == null) return;
 
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcWall")
+            
+            comboBox.Items.Clear();
+            
+            
+           // char[] somechar=new char[] {' '};
+           // s.CopyTo(31, somechar, 17, 7);
+            textBlock.Text = "you have chosen "+s+", now choose one of the "+s+" from comboBox to see its properties.";
+            if (s == "Walls")
             {
-                comboBox.Items.Clear();
+               
                 for (int i = 0; i < list.Count; i++)
                 {
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcWall;
                    
                     comboBox.Items.Add(one.ToString());
                 }
+                
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcBeam")
+            if (s == "Beams")
             {
-                comboBox.Items.Clear();
+                
                 for (int i = 0; i < list.Count; i++)
                 {
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcBeam;
@@ -101,51 +112,58 @@ namespace TUM.CMS.VplControl.Test.Nodes
                 }
             }
 
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcColumn")
+            if (s == "Columns")
             {
                 for (int i = 0; i < list.Count; i++)
                 {
-                    comboBox.Items.Clear();
+                    
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcColumn;
                     comboBox.Items.Add(one.ToString());
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcDoor")
+            if (s == "Doors")
             {
                 for (int i = 0; i < list.Count; i++)
                 {
-                    comboBox.Items.Clear();
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcDoor; 
                     comboBox.Items.Add(one.ToString());
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcPlate")
+            if (s == "Plates")
             {
                 for (int i = 0; i < list.Count; i++)
                 {
-                    comboBox.Items.Clear();
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcPlate;
                     comboBox.Items.Add(one.ToString());
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcSlab")
+            if (s == "Slabs")
             {
                 for (int i = 0; i < list.Count; i++)
                 {
-                    comboBox.Items.Clear();
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcSlab;
                     comboBox.Items.Add(one.ToString());
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcStair")
+            if (s == "Stairs")
             {
-                comboBox.Items.Clear();
                 for (int i = 0; i < list.Count; i++)
                 {
                     var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcStair;
                     comboBox.Items.Add(one.ToString());
                 }
             }
+            if (s == "CurtainWalls")
+            {
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    var one = list[i] as Xbim.Ifc2x3.SharedBldgElements.IfcCurtainWall;
+
+                    comboBox.Items.Add(one.ToString());
+                }
+            }
+            
             if (s == "")
             { textBlock.Text = "not found!"; }
 
@@ -161,57 +179,63 @@ namespace TUM.CMS.VplControl.Test.Nodes
             if (t.IsGenericType)
             {
                 var list = InputPorts[0].Data as IList;
-                if (list == null) return s;
+                if (list.Count==0) return s;
 
                 string p = list[0].GetType().ToString();
                 Match m = Regex.Match(p, "Wall");
                 if(m.Success)
                 {  
-                    s= "Xbim.Ifc2x3.SharedBldgElements.IfcWall";
+                    s= "Walls";
                 }
                 m = Regex.Match(p, "Beam");
                 if(m.Success )
                 {
-                    s= "Xbim.Ifc2x3.SharedBldgElements.IfcBeam";
+                    s= "Beams";
                 }
                 m = Regex.Match(p, "Column");
                 if (m.Success)
                 {
-                    s = "Xbim.Ifc2x3.SharedBldgElements.IfcColumn";
+                    s = "Columns";
                 }
                 m = Regex.Match(p, "Door");
                 if (m.Success)
                 {
-                    s = "Xbim.Ifc2x3.SharedBldgElements.IfcDoor";
+                    s = "Doors";
                 }
                 m = Regex.Match(p, "Plate");
                 if (m.Success)
                 {
-                    s = "Xbim.Ifc2x3.SharedBldgElements.IfcPlate";
+                    s = "Plates";
                 }
                 m = Regex.Match(p, "Slab");
                 if (m.Success)
                 {
-                    s = "Xbim.Ifc2x3.SharedBldgElements.IfcSlab";
+                    s = "Slabs";
                 }
                 m = Regex.Match(p, "Stair");
                 if (m.Success)
                 {
-                    s = "Xbim.Ifc2x3.SharedBldgElements.IfcStair";
+                    s = "Stairs";
+                }
+                m = Regex.Match(p, "CurtainWall");
+                if (m.Success)
+                {
+                    s = "CurtainWalls";
                 }
 
             }
             return s;
         }
-          private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+          private void comboBox_selectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            
             var scrollViewer = ControlElements[0] as ScrollViewer;
             if (scrollViewer == null) return;
             var textBlock = scrollViewer.Content as TextBlock;
             if (textBlock == null) return;
             var comboBox = ControlElements[2] as ComboBox;
             if (comboBox == null) return;
+            if(comboBox .SelectedIndex ==-1) return;
 
             var list = InputPorts[0].Data as IList;
             if (list == null) return;
@@ -219,7 +243,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
             string s = inputDataConvertion();
            
 
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcWall")
+            if (s == "Walls")
             { var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcWall;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
                 var ifcwallpropertyset = one.PropertySets.ToList();
@@ -231,7 +255,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
                     textBlock.Text += ifcpropertyname.ToString() + "  "+ifcpropertyvalue.ToString() + "\n\r";
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcBeam")
+            if (s == "Beams")
             {
                 var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcBeam;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
@@ -244,7 +268,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
                     textBlock.Text += ifcpropertyname.ToString() + "  " + ifcpropertyvalue.ToString() + "\n\r";
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcColumn")
+            if (s == "Columns")
             {
                 var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcColumn;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
@@ -257,7 +281,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
                     textBlock.Text += ifcpropertyname.ToString() + "  " + ifcpropertyvalue.ToString() + "\n\r";
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcDoor")
+            if (s == "Doors")
             {
                 var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcDoor;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
@@ -270,7 +294,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
                     textBlock.Text += ifcpropertyname.ToString() + "  " + ifcpropertyvalue.ToString() + "\n\r";
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcPlate")
+            if (s == "Plates")
             {
                 var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcPlate;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
@@ -283,7 +307,7 @@ namespace TUM.CMS.VplControl.Test.Nodes
                     textBlock.Text += ifcpropertyname.ToString() + "  " + ifcpropertyvalue.ToString() + "\n\r";
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcSlab")
+            if (s == "Slabs")
             {
                 var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcSlab;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
@@ -296,9 +320,22 @@ namespace TUM.CMS.VplControl.Test.Nodes
                     textBlock.Text += ifcpropertyname.ToString() + "  " + ifcpropertyvalue.ToString() + "\n\r";
                 }
             }
-            if (s == "Xbim.Ifc2x3.SharedBldgElements.IfcStair")
+            if (s == "Stairs")
             {
                 var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcStair;
+                textBlock.Text = one.Name.ToString() + " has \n\r";
+                var ifcwallpropertyset = one.PropertySets.ToList();
+                var ifchasproperties = ifcwallpropertyset[0].HasProperties.ToList();
+                for (int k = 0; k < ifchasproperties.Count; k++)
+                {
+                    var ifcpropertyname = ifchasproperties[k].Name;
+                    var ifcpropertyvalue = ifchasproperties[k];
+                    textBlock.Text += ifcpropertyname.ToString() + "  " + ifcpropertyvalue.ToString() + "\n\r";
+                }
+            }
+            if (s == "CurtainWalls")
+            {
+                var one = list[comboBox.SelectedIndex] as Xbim.Ifc2x3.SharedBldgElements.IfcCurtainWall;
                 textBlock.Text = one.Name.ToString() + " has \n\r";
                 var ifcwallpropertyset = one.PropertySets.ToList();
                 var ifchasproperties = ifcwallpropertyset[0].HasProperties.ToList();
