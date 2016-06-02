93d94c6a03e8022168844f623b3edd5c8ec3a120
Cornelius Preidel <cornelius.preidel@googlemail.com>
2016-06-02 17:08:10 +0200

    Geometry of an IFC Object can now be parsed - Visualization does not work yet.

--- src/TUM.CMS.VplControl.IFC/Nodes/IfcParseGeometryNode.cs
+++ src/TUM.CMS.VplControl.IFC/Nodes/IfcParseGeometryNode.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using Microsoft.Win32;
+using System.Windows.Media;
 using TUM.CMS.VplControl.Core;
 using Xbim.IO;
 using Xbim.ModelGeometry.Scene;
-using Xbim.Presentation;
 using Xbim.XbimExtensions;
 using XbimGeometry.Interfaces;
-using System.Linq;
-using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
 using Xbim.Geometry.Engine.Interop;
+using Xbim.Ifc2x3.Extensions;
 using Xbim.Ifc2x3.GeometricModelResource;
+using Xbim.Ifc2x3.GeometryResource;
+using Xbim.ModelGeometry.Scene.Extensions;
 
 namespace TUM.CMS.VplControl.IFC.Nodes
 {
@@ -22,32 +23,20 @@ namespace TUM.CMS.VplControl.IFC.Nodes
     {
         public XbimModel xModel;
 
+        private HelixViewport3D viewPort;
+
         public IfcParseGeometryNode(Core.VplControl hostCanvas) : base(hostCanvas)
         {
+            // Init UI
+            IsResizeable = true;
+
             AddInputPortToNode("Test", typeof(string));
 
             AddOutputPortToNode("Test", typeof(string));
 
-            var label = new Label
-            {
-                Content = "IFC File Rading",
-                Width = 100,
-                FontSize = 15,
-                HorizontalContentAlignment = HorizontalAlignment.Center
-            };
-            var textBlock = new TextBlock
-            {
-                TextWrapping = TextWrapping.Wrap,
-                FontSize = 10,
-                Padding = new Thickness(5),
-                IsHitTestVisible = false
-            };
-            AddControlToNode(label);
-            AddControlToNode(textBlock);
-
-
+            viewPort = new HelixViewport3D();
+            AddControlToNode(viewPort);
         }
-
        
         public override void Calculate()
         {
@@ -60,34 +49,51 @@ namespace TUM.CMS.VplControl.IFC.Nodes
                 var path = Path.GetTempPath();
                 xModel = new XbimModel();
                 xModel.CreateFrom(file, path + "temp_reader" + number + ".xbim");
+                xModel.Close();
+
+
+                var res = xModel.Open(path + "temp_reader" + number + ".xbim", XbimDBAccess.ReadWrite);
+
+                if (res == false)
+                {
+                    var err = xModel.Validate(TextWriter.Null, ValidationFlags.All);
+                    MessageBox.Show("ERROR in reading process!");
+                }
 
                 IXbimSolid solid;
                 XbimGeometryEngine _xbimGeometryCreator = new XbimGeometryEngine();
-                if(xModel.Instances.OfType<IfcExtrudedAreaSolid>() != null)
+
+                foreach (var item in xModel.Instances.OfType<IfcExtrudedAreaSolid>())
                 {
-                    var instance = xModel.Instances.OfType<IfcExtrudedAreaSolid>().FirstOrDefault();
-                    solid = _xbimGeometryCreator.CreateSolid(instance);
+                    solid = _xbimGeometryCreator.CreateSolid(item);
+                    var fbrep = _xbimGeometryCreator.CreateShapeGeometry(solid, 0.01, 10);
+
+                    // IXbimGeometryObject
+
+                    // Output on console - WORKS!
+                    // foreach (var vertex in mesh.Positions)
+                    // {
+                    //     Console.WriteLine(vertex.X + ", " + vertex.Y + ", " + vertex.Z);
+                    // }
+                    // 
+                    // for (int i = 0; i < mesh.TriangleIndices.Count; i += 3)
+                    // {
+                    //     Console.WriteLine(mesh.TriangleIndices[i] + ", " + mesh.TriangleIndices[i + 1] + ", " + mesh.TriangleIndices[i + 2]);
+                    // }
+                    // 
+                    // Console.ReadLine();
+
+                    XbimMeshGeometry3D mesh = new XbimMeshGeometry3D();
+
+                    // mesh.Read()
+                    mesh.Read(fbrep.ShapeData.ToString());
+                    mesh.Read(System.Text.Encoding.ASCII.GetString(fbrep.ShapeData));
+
+                    // Use the Mesh Builder now to create a Geometry, which can be viewed by the Helix Toolkit -> MeshBuilder
+                    // Create the meshbuilder
+                    var meshBuilder = new MeshBuilder(false, false);
+                    CreateMesh(meshBuilder, mesh);
                 }
-
-
-
-               // XbimShapeGeometry fbrep = _xbimGeometryCreator.CreateShapeGeometry(solid, 0.01, 10);
-               // XbimMeshGeometry3D mesh = new XbimMeshGeometry3D();
-               // mesh.Read(fbrep.ShapeData);
-               // 
-               // foreach (var vertex in mesh.Positions)
-               // {
-               //     Console.WriteLine(vertex.X + ", " + vertex.Y + ", " + vertex.Z);
-               // }
-               // 
-               // for (int i = 0; i < mesh.TriangleIndices.Count; i += 3)
-               // {
-               //     Console.WriteLine(mesh.TriangleIndices[i] + ", " + mesh.TriangleIndices[i + 1] + ", " + mesh.TriangleIndices[i + 2]);
-               // }
-               // 
-               // Console.ReadLine();
-
-
             }
         }
 
@@ -100,7 +106,44 @@ namespace TUM.CMS.VplControl.IFC.Nodes
             };
         }
 
+        /// <summary>
+        /// CreateMesh
+        /// </summary>
+        /// <param name="meshBuilder"></param>
+        /// <param name="mesh"></param>
+        public void CreateMesh(MeshBuilder meshBuilder, XbimMeshGeometry3D mesh)
+        {
+            // Output on console
+            var points = new List<Point3D>();
 
+            foreach (var item in mesh.Positions)
+            {
+                points.Add(new Point3D { X = item.X, Y = item.Y, Z = item.Z });
+            }
 
+            for (int i = 0; i < mesh.TriangleIndices.Count; i += 3)
+            {
+                meshBuilder.AddTriangle(points[mesh.TriangleIndices[i]], points[mesh.TriangleIndices[i + 1]],
+                            points[mesh.TriangleIndices[i + 2]]);
+            }
+
+            // Create the Geometry
+            var myGeometryModel = new GeometryModel3D
+            {
+                Material = new DiffuseMaterial(new SolidColorBrush(Colors.Aqua)),
+                BackMaterial = new DiffuseMaterial(new SolidColorBrush(Colors.Red)),
+                Geometry = meshBuilder.ToMesh(true)
+                // In case that you have to rotate the model ... 
+                // Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), 90))
+            };
+
+            var element = new ModelUIElement3D { Model = myGeometryModel };
+
+            // Click event Handler
+            // element.MouseDown += (sender1, e1) => OnElementMouseDown(sender1, e1, this);
+
+            // Add the Mesh to the ViewPort
+            viewPort.Children.Add(element);
+        }
     }
 }
