c44bfbb24d24b2293aaa35e571311382775d4146
WangYini <yini.wang@tum.de>
2016-06-19 18:27:35 +0200

    prepare the needed property values for energy node

--- src/TUM.CMS.VplControl.Energy/Nodes/EnergyNode.cs
+++ src/TUM.CMS.VplControl.Energy/Nodes/EnergyNode.cs
@@ -3,11 +3,22 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Xml;
 using TUM.CMS.VplControl.Core;
+using TUM.CMS.VplControl.IFC.Utilities;
+using System.Collections.Generic;
+using Xbim.IO;
+using Xbim.XbimExtensions;
+using System.Linq;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using Xbim.Ifc2x3.ProductExtension;
+using System.IO;
 
 namespace TUM.CMS.VplControl.Energy.Nodes
 {
     public class EnergyNode : Node
     {
+        public XbimModel xModel;
         public EnergyNode(Core.VplControl hostCanvas)
             : base(hostCanvas)
         {
@@ -26,8 +37,32 @@ namespace TUM.CMS.VplControl.Energy.Nodes
         }
         public override void Calculate()
         {
+            ///
+            /// ifc wall thickness calculation, saved in ifcWallThickness list, prepare for energy node
+            ///
+            var modelid = ((ModelInfo)(InputPorts[0].Data)).ModelId;
+            if (modelid == null) return;
+            xModel = DataController.Instance.GetModel(modelid);
+            var ifcWall = xModel.IfcProducts.OfType<Xbim.Ifc2x3.SharedBldgElements.IfcWall>().ToList();
+            List<double> ifcWallThickness = new List<double> { };
+            for (int i = 0; i < ifcWall.Count; i++)
+            {
+                var ifcWallVolume = ifcWall[i].PropertySets.ToList()[1].HasProperties.ToList()[2];
+                var ifcWallArea = ifcWall[i].PropertySets.ToList()[1].HasProperties.ToList()[0];
+                var volume = ifcWallVolume as Xbim.Ifc2x3.PropertyResource.IfcPropertySingleValue;
+                var volumeValue = volume.NominalValue as Xbim.XbimExtensions.SelectTypes.IfcValue;
+                object volumeValueTrue = volume.NominalValue.Value;
+                double volumeVal = (double)volumeValueTrue;
+                var area = ifcWallArea as Xbim.Ifc2x3.PropertyResource.IfcPropertySingleValue;
+                var areaValue = area.NominalValue as Xbim.XbimExtensions.SelectTypes.IfcValue;
+                object areaValueTrue = area.NominalValue.Value;
+                double areaVal = (double)areaValueTrue;
+                ifcWallThickness.Add(volumeVal / areaVal);
+
+            }
             
-        }
+            
+            }
 
         public override void SerializeNetwork(XmlWriter xmlWriter)
         {
