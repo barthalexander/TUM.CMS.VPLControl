e9d498ee83497460eab611b8950096d9e721de34
WangYini <yini.wang@tum.de>
2016-10-27 22:43:53 +0200

    refactory

---
+++ src/TUM.CMS.VplControl.IFC/Nodes/IfcMvdfilter.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Controls;
+using System.Xml;
+using TUM.CMS.VplControl.Core;
+using System.Collections.Generic;
+using Xbim.IO;
+using Xbim.XbimExtensions;
+using TUM.CMS.VplControl.IFC.Utilities;
+using System.Linq;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System;
+using System.Windows;
+using System.Reflection;
+using Xbim.Ifc2x3.ProductExtension;
+using System.IO;
+using Xbim.Ifc2x3.UtilityResource;
+using Xbim.Ifc2x3.SharedBldgElements;
+
+
+namespace TUM.CMS.VplControl.IFC.Nodes
+{
+    public class IfcMvdFilter : Node
+    {
+        
+        
+        public List<ModelInfo> ModelList;
+        private XbimModel _xModel;
+
+        
+
+        public IfcMvdFilter(Core.VplControl hostCanvas)
+            : base(hostCanvas)
+        {
+
+            AddInputPortToNode("Mvd", typeof(object),true);
+            AddInputPortToNode("Ifc", typeof(object), true);
+
+            AddOutputPortToNode("FilteredProducts", typeof(object));
+
+           // AddControlToNode();
+
+    
+
+        }
+
+        public override void Calculate()
+        {
+            ModelView modelView = (ModelView)InputPorts[0].Data;
+            List<ModelInfo> modelList = (List<ModelInfo>)InputPorts[1].Data;
+            Dictionary<string, ConceptRoot> roots= modelView.GetRoots();
+           // rootsValue = roots.Values.ToString();
+
+        }
+
+        private void doWork(XbimModel xModel, int indexOfModel, List<IfcGloballyUniqueId> elementIdsList)
+        {
+        }
+
+      
+
+        public override void SerializeNetwork(XmlWriter xmlWriter)
+        {
+            base.SerializeNetwork(xmlWriter);
+
+            // add your xml serialization methods here
+        }
+
+        public override void DeserializeNetwork(XmlReader xmlReader)
+        {
+            base.DeserializeNetwork(xmlReader);
+
+            // add your xml deserialization methods here
+        }
+
+        public override Node Clone()
+        {
+            return new IfcMvdFilter(HostCanvas)
+            {
+                Top = Top,
+                Left = Left
+            };
+        }
+    }
+}
---
+++ src/TUM.CMS.VplControl.IFC/Utilities/MvdInfo.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using Xbim.Ifc2x3.UtilityResource;
+
+namespace TUM.CMS.VplControl.IFC.Utilities
+{
+    public class Templates
+    {
+        Dictionary<string, ConceptTemplate> conceptTemplates = new Dictionary<string, ConceptTemplate>();
+
+        public void addConceptTemplate(string uuid, ConceptTemplate conceptTemplate)
+        {
+            conceptTemplates.Add(uuid, conceptTemplate);
+        }
+
+        public ConceptTemplate getConceptTemplate(string uuid)
+        {
+            return conceptTemplates[uuid];
+        }
+    }
+
+    public class ConceptTemplate
+    {
+        private string uuid;
+        public string name { get; set; }
+        public string code { get; set; }
+
+        List<Definition> definitiones = new List<Definition>();
+        Dictionary<string, ConceptTemplate> subConceptTemplateses = new Dictionary<string, ConceptTemplate>();
+
+        public void addDefinition(Definition definition)
+        {
+            definitiones.Add(definition);
+        }
+        public void addConceptTemplates(string uuid, ConceptTemplate conceptTemplate)
+        {
+            subConceptTemplateses.Add(uuid, conceptTemplate);
+        }
+
+        public void setUUID(string uuid)
+        {
+            this.uuid = uuid;
+        }
+
+        public string getUUID()
+        {
+            return uuid;
+        }
+        public ConceptTemplate getConceptTemplate(string uuid)
+        {
+            return subConceptTemplateses[uuid];
+        }
+    }
+
+
+    public class Definition
+    {
+        string Body;
+        List<string> Link = new List<string>();
+
+        public void addLink(string link)
+        {
+            Link.Add(link);
+        }
+
+        public void setBody(string body)
+        {
+            this.Body = body;
+        }
+
+
+    }
+
+    public class ModelView
+    {
+        public string uuid { get; set; }
+        public string name { get; set; }
+
+        List<Definition> definitiones = new List<Definition>();
+        Dictionary<string, ExchangeRequirement> exchangeRequirements = new Dictionary<string, ExchangeRequirement>();
+        Dictionary<string, ConceptRoot> roots = new Dictionary<string, ConceptRoot>();
+
+        public void addExchangeRequirements(string uuid, ExchangeRequirement exchangeRequirement)
+        {
+            exchangeRequirements.Add(uuid, exchangeRequirement);
+        }
+
+        public ExchangeRequirement GetExchangeRequirement(string uuid)
+        {
+            return exchangeRequirements[uuid];
+        }
+
+        public Dictionary<string, ConceptRoot> GetRoots()
+        {
+            return roots;
+        }
+
+        public Dictionary<string, ExchangeRequirement> GetAllER()
+        {
+            return exchangeRequirements;
+        }
+
+        public void addRoots(string uuid, ConceptRoot conceptRoot)
+        {
+            roots.Add(uuid, conceptRoot);
+        }
+
+        public void addDefinition(Definition definition)
+        {
+            definitiones.Add(definition);
+        }
+
+    }
+
+    public class ConceptRoot
+    {
+        public string uuid { get; set; }
+        public string name { get; set; }
+        public string applicableRootEntity { get; set; }
+        List<Definition> definitiones = new List<Definition>();
+        Dictionary<string, Concept> concepts = new Dictionary<string, Concept>();
+
+        public void addDefinition(Definition definition)
+        {
+            definitiones.Add(definition);
+        }
+        public void addConcept(string uuid, Concept concept)
+        {
+            concepts.Add(uuid, concept);
+        }
+
+    }
+
+    public class ExchangeRequirement
+    {
+        public string uuid { get; set; }
+        public string name { get; set; }
+        List<Definition> definitiones = new List<Definition>();
+        public void addDefinition(Definition definition)
+        {
+            definitiones.Add(definition);
+        }
+    }
+
+    public class Concept
+    {
+        public string uuid { get; set; }
+        public string name { get; set; }
+        ConceptTemplate conceptTemplate = new ConceptTemplate();
+        List<Definition> definitiones = new List<Definition>();
+        Dictionary<string, Requirement> requirements = new Dictionary<string, Requirement>();
+        Dictionary<string, Concept> subConcepts = new Dictionary<string, Concept>();
+
+        public void addDefinition(Definition definition)
+        {
+            definitiones.Add(definition);
+        }
+
+        public void addRequirements(string value, Requirement requirement)
+        {
+            requirements.Add(value, requirement);
+        }
+
+        public void addSubConcept(string uuid, Concept concept)
+        {
+            subConcepts.Add(uuid, concept);
+        }
+
+        public void setConceptTemplate(ConceptTemplate conceptTemplate)
+        {
+            this.conceptTemplate = conceptTemplate;
+        }
+
+        public ConceptTemplate getConceptTemplate()
+        {
+            return conceptTemplate;
+        }
+    }
+
+    public class Requirement
+    {
+        public string applicability;
+        public ExchangeRequirement exchangeRequirement;
+        public string requirement;
+    }
+}
